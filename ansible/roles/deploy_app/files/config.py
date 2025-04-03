fa075ccedaab8d369dc9120aa025fd3e9e00f47a
joannedada <joanneobodoagwu@gmail.com>
2025-04-03 18:13:14 +0100

    postgresql

--- ansible/roles/deploy_app/files/config.py
+++ ansible/roles/deploy_app/files/config.py
@@ -1,6 +1,6 @@
 import os
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_HOST = os.getenv("DB_HOST", "172.31.81.215")
 DB_NAME = os.getenv("DB_NAME", "devopsdb")
-DB_USER = os.getenv("DB_USER", "devops")
-DB_PASS = os.getenv("DB_PASS", "password")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "postgres")
