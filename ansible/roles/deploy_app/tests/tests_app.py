7b0164c67d5f50dea84435927fde694d6b87b46d
joannedada <joanneobodoagwu@gmail.com>
2025-04-07 00:38:48 +0100

    pytests

--- ansible/roles/deploy_app/tests/tests_app.py
+++ ansible/roles/deploy_app/tests/tests_app.py
@@ -5,4 +5,4 @@ def test_home_route():
     """Test if home route returns 200 OK"""
     with app.test_client() as client:
         response = client.get('/')
-        assert response.status_code == 200
+        assert response.status_code == 200  # nosec B101
