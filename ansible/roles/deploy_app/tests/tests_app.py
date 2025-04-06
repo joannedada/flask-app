14ca130752f666e00f7b3648fa4306d7b61c71f7
joannedada <joanneobodoagwu@gmail.com>
2025-04-07 00:28:00 +0100

    tests_app.py

--- ansible/roles/deploy_app/tests/tests_app.py
+++ ansible/roles/deploy_app/tests/tests_app.py
@@ -1,7 +1,10 @@
 from app import app
 
+
 def test_home_route():
     """Test if home route returns 200 OK"""
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
+
+        
